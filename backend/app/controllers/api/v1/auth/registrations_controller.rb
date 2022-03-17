cc841adabef63eb75eda4214f164d161ff650e47
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-17 16:05:47 +0900

    rails  reg_cont にaccount_update_params追加

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     def sign_up_params
       params.permit(:email, :password, :password_confirmation, :name)
     end
+
+    def account_update_params
+      params.permit(:email, :name)
+    end
 end
