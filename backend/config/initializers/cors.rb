edaf0380f55d7f9d8089f6cc3bc43223eab49954
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-21 15:15:51 +0900

    Update cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000" ,"54.249.116.120:3000"
+    origins "54.249.116.120:3000"
 
     resource "*",
       headers: :any,
