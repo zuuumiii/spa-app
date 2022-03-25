0075abbf05916e9b4e00fdec3b3890e139445e34
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-25 12:24:19 +0900

    fieldsコントローラーcreate追加

--- backend/app/controllers/fields_controller.rb
+++ backend/app/controllers/fields_controller.rb
@@ -1,10 +1,23 @@
-class FieldsController < ApplicationController
-
-  def create 
-
+module Api
+  module V1
+  
+    class FieldsController < ApplicationController
+    
+      def create 
+       field = Field.new(field_params)
+       if field.save
+        render json: { status: "SUCCESS", data: field }
+       else
+        render json: { status: "ERROR", data: field.errors }
+       end
+      end
+    
+    private
+      def field_params
+        params.permit(:field_name, :product, :info, :area, :start_date, :correct).merge(user_id: current_api_v1_user.id)
+      end
+    
+    
+    end
   end
-
-
-
-
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :fields, dependent: :destroy, 
+  has_many :fields, dependent: :destroy
 end
