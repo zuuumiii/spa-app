2abd4a5a2a91cfacddf0b30863ea12ebf87eeeda
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-29 12:35:10 +0900

    createアクション追加

--- backend/app/controllers/api/v1/targets_controller.rb
+++ backend/app/controllers/api/v1/targets_controller.rb
@@ -2,7 +2,19 @@ module Api
   module V1
     class TargetsController < ApplicationController
     
-    
+      def create 
+        target = Target.new(target_params)
+        if target.save
+          render json: { status: "SUCCESS", data: target }
+        else
+          render json: { status: "ERROR", data: target.errors }
+        end
+      end
+
+      private
+      def target_params
+        params.permit(:target_name, :target_temp, :memo).merge(user_id: current_api_v1_user.id, target_id: params[:field_id])
+      end
     end
   end
 end
