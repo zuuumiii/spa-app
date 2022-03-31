5c8eb9c0f3e6e66bf27725da4b2ad96a26e4553d
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-03-31 22:02:50 +0900

    Update client.ts

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -1,18 +1,16 @@
 import applyCaseMiddleware from "axios-case-converter";
 import axios from "axios";
 
-// applyCaseMiddleware:
-// axiosで受け取ったレスポンスの値をスネークケース→キャメルケースに変換
-// または送信するリクエストの値をキャメルケース→スネークケースに変換してくれるライブラリ
+//スネークケース→キャメルケースに変換
 
-// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
+// ヘッダーに関しては無視するオプションを追加
 const options = {
   ignoreHeaders: true,
 };
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "http://54.249.116.120:3001/api/v1",
   }),
   options
 );
