33e751cdaa080b9a7a100a6d00e1fae1599c957d
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-01 23:13:32 +0900

    観測所リスト完成

--- frontend/src/components/precblock/PrefBlockList.tsx
+++ frontend/src/components/precblock/PrefBlockList.tsx
@@ -180,4 +180,810 @@ export const PrecBlockList: PrecBlock[] = [
       },
     ],
   },
+  {
+    precNo: 31,
+    precName: "青森県",
+    blocks: [
+      {
+        blockNo: 47575,
+        blockName: "青森",
+      },
+      {
+        blockNo: 47574,
+        blockName: "深浦",
+      },
+      {
+        blockNo: 47581,
+        blockName: "八戸",
+      },
+    ],
+  },
+  {
+    precNo: 32,
+    precName: "秋田県",
+    blocks: [
+      {
+        blockNo: 47582,
+        blockName: "秋田",
+      },
+    ],
+  },
+  {
+    precNo: 33,
+    precName: "岩手県",
+    blocks: [
+      {
+        blockNo: 47584,
+        blockName: "盛岡",
+      },
+      {
+        blockNo: 47585,
+        blockName: "宮古",
+      },
+      {
+        blockNo: 47512,
+        blockName: "大船渡",
+      },
+    ],
+  },
+  {
+    precNo: 34,
+    precName: "宮城県",
+    blocks: [
+      {
+        blockNo: 47592,
+        blockName: "石巻",
+      },
+      {
+        blockNo: 47590,
+        blockName: "仙台",
+      },
+    ],
+  },
+  {
+    precNo: 35,
+    precName: "山形県",
+    blocks: [
+      {
+        blockNo: 47587,
+        blockName: "酒田",
+      },
+      {
+        blockNo: 47520,
+        blockName: "新庄",
+      },
+      {
+        blockNo: 47588,
+        blockName: "山形",
+      },
+    ],
+  },
+  {
+    precNo: 36,
+    precName: "福島県",
+    blocks: [
+      {
+        blockNo: 47595,
+        blockName: "福島",
+      },
+      {
+        blockNo: 47570,
+        blockName: "若松",
+      },
+      {
+        blockNo: 47597,
+        blockName: "白川",
+      },
+      {
+        blockNo: 47598,
+        blockName: "小名浜",
+      },
+    ],
+  },
+  {
+    precNo: 40,
+    precName: "茨城県",
+    blocks: [
+      {
+        blockNo: 47629,
+        blockName: "水戸",
+      },
+      {
+        blockNo: 47646,
+        blockName: "つくば（館野）",
+      },
+    ],
+  },
+  {
+    precNo: 41,
+    precName: "栃木県",
+    blocks: [
+      {
+        blockNo: 47615,
+        blockName: "宇都宮",
+      },
+      {
+        blockNo: 47690,
+        blockName: "奥日光（日光）",
+      },
+    ],
+  },
+  {
+    precNo: 42,
+    precName: "群馬県",
+    blocks: [
+      {
+        blockNo: 47624,
+        blockName: "前橋",
+      },
+    ],
+  },
+  {
+    precNo: 43,
+    precName: "埼玉県",
+    blocks: [
+      {
+        blockNo: 47626,
+        blockName: "熊谷",
+      },
+      {
+        blockNo: 47641,
+        blockName: "秩父",
+      },
+    ],
+  },
+  {
+    precNo: 44,
+    precName: "東京都",
+    blocks: [
+      {
+        blockNo: 47662,
+        blockName: "東京",
+      },
+      {
+        blockNo: 47675,
+        blockName: "大島",
+      },
+      {
+        blockNo: 47677,
+        blockName: "三宅島",
+      },
+      {
+        blockNo: 47678,
+        blockName: "八丈島",
+      },
+      {
+        blockNo: 47971,
+        blockName: "父島",
+      },
+      {
+        blockNo: 47991,
+        blockName: "南鳥島",
+      },
+    ],
+  },
+  {
+    precNo: 45,
+    precName: "千葉県",
+    blocks: [
+      {
+        blockNo: 47648,
+        blockName: "銚子",
+      },
+      {
+        blockNo: 47682,
+        blockName: "千葉",
+      },
+      {
+        blockNo: 47674,
+        blockName: "勝浦",
+      },
+      {
+        blockNo: 47672,
+        blockName: "館山",
+      },
+    ],
+  },
+  {
+    precNo: 46,
+    precName: "神奈川県",
+    blocks: [
+      {
+        blockNo: 47670,
+        blockName: "横浜",
+      },
+    ],
+  },
+  {
+    precNo: 48,
+    precName: "長野県",
+    blocks: [
+      {
+        blockNo: 47610,
+        blockName: "長野",
+      },
+      {
+        blockNo: 47622,
+        blockName: "軽井沢",
+      },
+      {
+        blockNo: 47618,
+        blockName: "松本",
+      },
+      {
+        blockNo: 47620,
+        blockName: "諏訪",
+      },
+      {
+        blockNo: 47637,
+        blockName: "飯田",
+      },
+    ],
+  },
+  {
+    precNo: 49,
+    precName: "山梨県",
+    blocks: [
+      {
+        blockNo: 47638,
+        blockName: "甲府",
+      },
+      {
+        blockNo: 47640,
+        blockName: "河口湖",
+      },
+      {
+        blockNo: 47639,
+        blockName: "富士山山頂",
+      },
+    ],
+  },
+  {
+    precNo: 50,
+    precName: "静岡県",
+    blocks: [
+      {
+        blockNo: 47657,
+        blockName: "三島",
+      },
+      {
+        blockNo: 47668,
+        blockName: "網代",
+      },
+      {
+        blockNo: 47656,
+        blockName: "静岡",
+      },
+      {
+        blockNo: 47655,
+        blockName: "御前崎",
+      },
+      {
+        blockNo: 47666,
+        blockName: "石廊崎",
+      },
+    ],
+  },
+  {
+    precNo: 51,
+    precName: "愛知県",
+    blocks: [
+      {
+        blockNo: 47636,
+        blockName: "名古屋",
+      },
+      {
+        blockNo: 47653,
+        blockName: "伊良湖",
+      },
+    ],
+  },
+  {
+    precNo: 52,
+    precName: "岐阜県",
+    blocks: [
+      {
+        blockNo: 47617,
+        blockName: "高山",
+      },
+      {
+        blockNo: 47632,
+        blockName: "岐阜",
+      },
+    ],
+  },
+  {
+    precNo: 53,
+    precName: "三重県",
+    blocks: [
+      {
+        blockNo: 47684,
+        blockName: "四日市",
+      },
+      {
+        blockNo: 47649,
+        blockName: "上野",
+      },
+      {
+        blockNo: 47651,
+        blockName: "津",
+      },
+      {
+        blockNo: 47663,
+        blockName: "尾鷲",
+      },
+    ],
+  },
+  {
+    precNo: 54,
+    precName: "新潟県",
+    blocks: [
+      {
+        blockNo: 47602,
+        blockName: "相川",
+      },
+      {
+        blockNo: 47604,
+        blockName: "新潟",
+      },
+      {
+        blockNo: 47612,
+        blockName: "高田",
+      },
+    ],
+  },
+  {
+    precNo: 55,
+    precName: "富山県",
+    blocks: [
+      {
+        blockNo: 47606,
+        blockName: "伏木",
+      },
+      {
+        blockNo: 47607,
+        blockName: "富山",
+      },
+    ],
+  },
+  {
+    precNo: 56,
+    precName: "石川県",
+    blocks: [
+      {
+        blockNo: 47600,
+        blockName: "輪島",
+      },
+      {
+        blockNo: 47605,
+        blockName: "金沢",
+      },
+    ],
+  },
+  {
+    precNo: 57,
+    precName: "福井県",
+    blocks: [
+      {
+        blockNo: 47616,
+        blockName: "福井",
+      },
+      {
+        blockNo: 47631,
+        blockName: "敦賀",
+      },
+    ],
+  },
+  {
+    precNo: 60,
+    precName: "滋賀県",
+    blocks: [
+      {
+        blockNo: 47761,
+        blockName: "彦根",
+      },
+      {
+        blockNo: 47751,
+        blockName: "伊吹山",
+      },
+    ],
+  },
+  {
+    precNo: 61,
+    precName: "京都府",
+    blocks: [
+      {
+        blockNo: 47750,
+        blockName: "舞鶴",
+      },
+      {
+        blockNo: 47759,
+        blockName: "京都",
+      },
+    ],
+  },
+  {
+    precNo: 62,
+    precName: "大阪府",
+    blocks: [
+      {
+        blockNo: 47772,
+        blockName: "大阪",
+      },
+    ],
+  },
+  {
+    precNo: 63,
+    precName: "兵庫県",
+    blocks: [
+      {
+        blockNo: 47747,
+        blockName: "豊岡",
+      },
+      {
+        blockNo: 47769,
+        blockName: "姫路",
+      },
+      {
+        blockNo: 47770,
+        blockName: "神戸",
+      },
+      {
+        blockNo: 47776,
+        blockName: "洲本",
+      },
+    ],
+  },
+  {
+    precNo: 64,
+    precName: "奈良県",
+    blocks: [
+      {
+        blockNo: 47780,
+        blockName: "奈良",
+      },
+    ],
+  },
+  {
+    precNo: 65,
+    precName: "和歌山県",
+    blocks: [
+      {
+        blockNo: 47777,
+        blockName: "和歌山",
+      },
+      {
+        blockNo: 47778,
+        blockName: "潮岬",
+      },
+    ],
+  },
+  {
+    precNo: 66,
+    precName: "岡山県",
+    blocks: [
+      {
+        blockNo: 47768,
+        blockName: "岡山",
+      },
+      {
+        blockNo: 47756,
+        blockName: "津山",
+      },
+    ],
+  },
+  {
+    precNo: 67,
+    precName: "広島県",
+    blocks: [
+      {
+        blockNo: 47767,
+        blockName: "福山",
+      },
+      {
+        blockNo: 47765,
+        blockName: "広島",
+      },
+      {
+        blockNo: 47766,
+        blockName: "呉",
+      },
+    ],
+  },
+  {
+    precNo: 68,
+    precName: "島根県",
+    blocks: [
+      {
+        blockNo: 47740,
+        blockName: "西郷",
+      },
+      {
+        blockNo: 47741,
+        blockName: "松江",
+      },
+    ],
+  },
+  {
+    precNo: 68,
+    precName: "鳥取県",
+    blocks: [
+      {
+        blockNo: 47747,
+        blockName: "境",
+      },
+      {
+        blockNo: 47769,
+        blockName: "鳥取",
+      },
+      {
+        blockNo: 47770,
+        blockName: "米子",
+      },
+    ],
+  },
+  {
+    precNo: 71,
+    precName: "徳島県",
+    blocks: [
+      {
+        blockNo: 47895,
+        blockName: "徳島",
+      },
+      {
+        blockNo: 47894,
+        blockName: "剣山",
+      },
+    ],
+  },
+  {
+    precNo: 72,
+    precName: "香川県",
+    blocks: [
+      {
+        blockNo: 47891,
+        blockName: "高松",
+      },
+      {
+        blockNo: 47890,
+        blockName: "多度津",
+      },
+    ],
+  },
+  {
+    precNo: 73,
+    precName: "愛媛県",
+    blocks: [
+      {
+        blockNo: 47887,
+        blockName: "松山",
+      },
+      {
+        blockNo: 47892,
+        blockName: "宇和島",
+      },
+    ],
+  },
+  {
+    precNo: 74,
+    precName: "高知県",
+    blocks: [
+      {
+        blockNo: 47893,
+        blockName: "高知",
+      },
+      {
+        blockNo: 47899,
+        blockName: "室戸岬",
+      },
+      {
+        blockNo: 47897,
+        blockName: "宿毛",
+      },
+      {
+        blockNo: 47898,
+        blockName: "清水",
+      },
+    ],
+  },
+  {
+    precNo: 81,
+    precName: "山口県",
+    blocks: [
+      {
+        blockNo: 47754,
+        blockName: "萩",
+      },
+      {
+        blockNo: 47784,
+        blockName: "山口",
+      },
+    ],
+  },
+  {
+    precNo: 82,
+    precName: "福岡県",
+    blocks: [
+      {
+        blockNo: 47809,
+        blockName: "飯塚",
+      },
+      {
+        blockNo: 47807,
+        blockName: "福岡",
+      },
+    ],
+  },
+  {
+    precNo: 83,
+    precName: "大分県",
+    blocks: [
+      {
+        blockNo: 47814,
+        blockName: "日田",
+      },
+      {
+        blockNo: 47815,
+        blockName: "大分",
+      },
+    ],
+  },
+  {
+    precNo: 84,
+    precName: "長崎県",
+    blocks: [
+      {
+        blockNo: 47800,
+        blockName: "厳原",
+      },
+      {
+        blockNo: 47805,
+        blockName: "平戸",
+      },
+      {
+        blockNo: 47817,
+        blockName: "長崎",
+      },
+      {
+        blockNo: 47818,
+        blockName: "雲仙岳",
+      },
+      {
+        blockNo: 47843,
+        blockName: "福江",
+      },
+    ],
+  },
+  {
+    precNo: 85,
+    precName: "佐賀県",
+    blocks: [
+      {
+        blockNo: 47813,
+        blockName: "佐賀",
+      },
+    ],
+  },
+  {
+    precNo: 86,
+    precName: "熊本県",
+    blocks: [
+      {
+        blockNo: 47819,
+        blockName: "熊本",
+      },
+      {
+        blockNo: 47821,
+        blockName: "阿蘇山",
+      },
+      {
+        blockNo: 47824,
+        blockName: "人吉",
+      },
+      {
+        blockNo: 47838,
+        blockName: "牛深",
+      },
+    ],
+  },
+  {
+    precNo: 86,
+    precName: "宮崎県",
+    blocks: [
+      {
+        blockNo: 47822,
+        blockName: "延岡",
+      },
+      {
+        blockNo: 47830,
+        blockName: "宮崎",
+      },
+      {
+        blockNo: 47829,
+        blockName: "都城",
+      },
+      {
+        blockNo: 47835,
+        blockName: "油津",
+      },
+    ],
+  },
+  {
+    precNo: 88,
+    precName: "鹿児島県",
+    blocks: [
+      {
+        blockNo: 47823,
+        blockName: "阿久根",
+      },
+      {
+        blockNo: 47827,
+        blockName: "鹿児島",
+      },
+      {
+        blockNo: 47831,
+        blockName: "枕崎",
+      },
+      {
+        blockNo: 47837,
+        blockName: "種子島",
+      },
+      {
+        blockNo: 47909,
+        blockName: "名瀬",
+      },
+      {
+        blockNo: 47836,
+        blockName: "屋久島",
+      },
+      {
+        blockNo: 47942,
+        blockName: "沖永良部",
+      },
+    ],
+  },
+  {
+    precNo: 91,
+    precName: "沖縄県",
+    blocks: [
+      {
+        blockNo: 47940,
+        blockName: "名護",
+      },
+      {
+        blockNo: 47936,
+        blockName: "那覇",
+      },
+      {
+        blockNo: 47918,
+        blockName: "石垣島",
+      },
+      {
+        blockNo: 47927,
+        blockName: "宮古島",
+      },
+      {
+        blockNo: 47929,
+        blockName: "久米島",
+      },
+      {
+        blockNo: 47917,
+        blockName: "西表島",
+      },
+      {
+        blockNo: 47912,
+        blockName: "与那国島",
+      },
+    ],
+  },
+  {
+    precNo: 99,
+    precName: "南極",
+    blocks: [
+      {
+        blockNo: 89532,
+        blockName: "昭和基地",
+      },
+    ],
+  },
 ];
