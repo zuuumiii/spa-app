22952c7e8ed4b738bbe596e4101ea53722342be3
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-04 22:59:44 +0900

    docker

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     
-    origins "localhost:3000" ,"54.249.116.120:3000"
+    origins "localhost:3000" ,"trag.online"
 
     resource "*",
       headers: :any,
