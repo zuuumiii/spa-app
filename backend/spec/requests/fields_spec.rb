a9f785c8f6ee0dabebccc554dcda7ce0d5177ffb
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-13 12:00:09 +0900

    requestテスト成功

--- backend/spec/requests/fields_spec.rb
+++ backend/spec/requests/fields_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Fields", type: :request do
-  describe "GET /fields" do
+  before do
+    @field = FactoryBot.create(:field)
+    @auth_tokens = auth_sign_in(@field.user)
+    @params = {
+      field_name: @field.field_name,
+      product: @field.product,
+      area: @field.area,
+      start_date: @field.start_date,
+      info: @field.info
+    }
+  end
+
+  describe "POST /fields" do
     it "works! (now write some real specs)" do
-      get fields_index_path
-      expect(response).to have_http_status(200)
+      post api_v1_fields_path, params: @params, headers: @auth_tokens
+
+      expect(response).to have_http_status :ok
     end
   end
 end
