e55e3b5feef8e87feb9dc2ebc59a7368cae6e090
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-13 18:06:29 +0900

    target削除リクエストテスト追加

--- backend/spec/requests/targets_spec.rb
+++ backend/spec/requests/targets_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Create", type: :request do
   before do
     @target = FactoryBot.create(:target)
-    @auth_tokens = auth_sign_in(@target.field.user)
+    @auth_tokens = auth_sign_in(@target.user)
     @params = {
       target_name: @target.target_name,
       target_temp: @target.target_temp,
@@ -35,4 +35,33 @@ RSpec.describe "Create", type: :request do
     end
   end
 
+  describe "目標削除" do
+    context "正しく削除できるとき" do
+      it "正しく削除できたらメッセージが含まれる" do
+        expect{delete api_v1_field_target_path(@target.field_id, @target.id), headers: @auth_tokens}.to change{Target.count}.by(-1)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("SUCCESS")
+        expect(res["data"]).to eq("削除しました")
+        expect(response).to have_http_status :ok
+      end
+    end
+    context "削除できないとき" do
+      it "別のユーザから削除しようとすると、エラーが出て削除されない" do
+        another_user = FactoryBot.create(:user)
+        another_tokens = auth_sign_in(another_user)
+        expect{delete api_v1_field_target_path(@target.field_id, @target.id), headers: another_tokens}.to change{Target.count}.by(0)
+        expect(Target.last.target_name).to eq(@target.target_name)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("ERROR")
+        expect(res["data"]).to eq("不正な操作です")
+        expect(response).to have_http_status :ok
+      end
+      it "認証されていないユーザーから削除できない" do
+        expect{delete api_v1_field_target_path(@target.field_id, @target.id), headers: nil}.to change{Target.count}.by(0)
+        res = JSON.parse(response.body)
+        expect(res["errors"]).to include("ログインもしくはアカウント登録してください。")
+        expect(response).to have_http_status 401
+      end
+    end
+  end
 end
