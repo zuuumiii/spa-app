5638113433dd1e4bdce409e118df3978639adf0f
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-13 15:29:29 +0900

    目標新規投稿リクエストテスト実装

--- backend/spec/requests/targets_spec.rb
+++ backend/spec/requests/targets_spec.rb
@@ -11,5 +11,28 @@ RSpec.describe "Create", type: :request do
     }
   end
 
+  describe "目標新規投稿" do
+    context "正しく入力されているとき" do
+      it "正しく投稿できる" do
+        expect{post api_v1_field_targets_path(@target.field), params: @params, headers: @auth_tokens}.to change{Target.count}.by(1)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("SUCCESS")
+        expect(res["data"]["target_name"]).to eq(Target.last.target_name)
+        expect(response).to have_http_status :ok
+      end
+    end
+
+    context "誤って入力されているとき" do
+      it "正しく投稿できずエラーが含まれている" do
+        @params["target_temp"] = 999999
+        expect{post api_v1_field_targets_path(@target.field), params: @params, headers: @auth_tokens}.to change{Target.count}.by(0)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("ERROR")
+        expect(res["data"]).to include("目標温度は10000より小さい値にしてください")
+        expect(res["data"]).not_to include(@target.target_temp)
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
 
 end
