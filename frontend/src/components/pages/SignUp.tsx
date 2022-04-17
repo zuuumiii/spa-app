50ab1cb7452580db755268c6e9ad300e149c645c
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-17 15:18:37 +0900

    新規登録ページにユーザー入力コンポーネント導入

--- frontend/src/components/pages/SignUp.tsx
+++ frontend/src/components/pages/SignUp.tsx
@@ -1,21 +1,16 @@
-import React, { useState, useContext, useRef } from "react";
+import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
 import Button from "@material-ui/core/Button";
 
 import { AuthContext } from "App";
 import AlertMessage from "components/alerts/AlertMessage";
 import { signUp } from "lib/api/auth";
 import { SignUpParams } from "interfaces/index";
-import PrecBlockBox, {
-  PrecBlockItem,
-} from "components/selectbox/precblock/PrecBlockBox";
+import UserForm from "../users/UserForm";
 import { PrecBlockList } from "components/selectbox/precblock/PrecBlockList";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -48,6 +43,14 @@ const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+  //選択中のprecNoをStateへ
+  const [selectedPrecNo, setSelectedPrecNo] = useState<number>(
+    PrecBlockList[0].precNo
+  );
+  //選択中のblockNoをStateへ
+  const [selectedBlockNo, setSelectedBlockNo] = useState<number>(
+    PrecBlockList[0].blocks[0].blockNo
+  );
   const [alertMessageOpen, setAlertMessageOpen] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -82,135 +85,62 @@ const SignUp: React.FC = () => {
       setAlertMessageOpen(true);
     }
   };
-  //Boxのアイテムとするprec一覧をStateへ
-  const [precOptions] = useState<PrecBlockItem[]>(
-    PrecBlockList.map((p) => {
-      return {
-        no: p.precNo,
-        name: p.precName,
-      };
-    })
-  );
-  //選択中のprecNoをStateへ
-  const [selectedPrecNo, setSelectedPrecNo] = useState<number>(
-    PrecBlockList[0].precNo
-  );
 
-  //選択中のprecに属するblockをRefに
-  const blockOptionsRef = useRef(
-    PrecBlockList.filter((p) => p.precNo === selectedPrecNo)[0].blocks.map(
-      (p) => {
-        return {
-          no: p.blockNo,
-          name: p.blockName,
-        };
-      }
-    )
-  );
-  //選択中のblockNoをStateへ
-  const [selectedBlockNo, setSelectedBlockNo] = useState<number>(
-    PrecBlockList[0].blocks[0].blockNo
-  );
-
-  const onPrecBoxChangeHandler = (precNo: number) => {
-    //選択したprecNoをStateへ
-    setSelectedPrecNo(precNo);
-    //選択したprecのblock一覧取得
-    const selectedPrecBlocks = PrecBlockList.filter(
-      (p) => p.precNo === precNo
-    )[0].blocks;
-    //選択した０番のblockをStateに指定
-    setSelectedBlockNo(selectedPrecBlocks[0].blockNo);
-
-    //選択したblockをRefに
-    blockOptionsRef.current = selectedPrecBlocks.map((p) => {
-      return {
-        no: p.blockNo,
-        name: p.blockName,
-      };
-    });
+  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const handleChangePasswordConfirmation = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPasswordConfirmation(e.target.value);
+  };
+  const handleChangePrecNo = (e: number) => {
+    setSelectedPrecNo(e);
+  };
+  const handleChangeBlockNo = (e: number) => {
+    setSelectedBlockNo(e);
   };
 
   return (
     <>
       <form noValidate autoComplete="off">
         <Card className={classes.card}>
-          <CardHeader className={classes.header} title="新規登録" />
-          <CardContent>
-            <TextField
-              name="name"
-              variant="outlined"
-              required
-              fullWidth
-              label="名前"
-              value={name}
-              margin="dense"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              name="email"
-              variant="outlined"
-              required
-              fullWidth
-              label="Email"
-              value={email}
-              margin="dense"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <PrecBlockBox
-              inputLabel="都道府県"
-              items={precOptions}
-              value={selectedPrecNo}
-              onChange={(selected) => onPrecBoxChangeHandler(selected)}
-            />
-            <PrecBlockBox
-              inputLabel="観測所"
-              items={blockOptionsRef.current}
-              value={selectedBlockNo}
-              onChange={(selected) => setSelectedBlockNo(selected)}
-            />
-
-            <TextField
-              name="password"
-              variant="outlined"
-              required
-              fullWidth
-              label="パスワード"
-              type="password"
-              value={password}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              name="password-confirmation"
-              variant="outlined"
-              required
-              fullWidth
-              label="確認用パスワード"
-              type="password"
-              value={passwordConfirmation}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              size="large"
-              fullWidth
-              color="default"
-              disabled={
-                !name || !email || !password || !passwordConfirmation
-                  ? true
-                  : false
-              }
-              className={classes.submitBtn}
-              onClick={handleSubmit}
-            >
-              登録
-            </Button>
-          </CardContent>
+          <UserForm
+            name={name}
+            email={email}
+            password={password}
+            passwordConfirmation={passwordConfirmation}
+            selectedPrecNo={selectedPrecNo}
+            selectedBlockNo={selectedBlockNo}
+            onChangePrecNo={handleChangePrecNo}
+            onChangeBlockNo={handleChangeBlockNo}
+            onChangeName={handleChangeName}
+            onChangeEmail={handleChangeEmail}
+            onChangePassword={handleChangePassword}
+            onChangePasswordConfirmaiton={handleChangePasswordConfirmation}
+          ></UserForm>
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            fullWidth
+            color="default"
+            disabled={
+              !name || !email || !password || !passwordConfirmation
+                ? true
+                : false
+            }
+            className={classes.submitBtn}
+            onClick={handleSubmit}
+          >
+            登録
+          </Button>
         </Card>
       </form>
       <AlertMessage
