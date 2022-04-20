032f2f90d08abbf1ee68f886f0c4bea6371e5dfd
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-20 16:37:23 +0900

    TargetCardのChangeイベントをまとめ、 各項目配列targetにまとめ

--- frontend/src/components/targets/TargetCard.tsx
+++ frontend/src/components/targets/TargetCard.tsx
@@ -54,13 +54,22 @@ interface Props {
 }
 
 const TargetCard: React.FC<Props> = (props) => {
-  const { onClickSubmit, onClickDelete }: Props = props;
+  const { target, onClickSubmit, onClickDelete }: Props = props;
   const classes = useStyles();
-  const [target, setTarget] = useState<TargetParams>(props.target);
+
+  const initialTargetUpdateParams: TargetCreateParams = {
+    targetName: target.targetName,
+    targetTemp: target.targetTemp,
+    memo: target.memo,
+  };
+  const [targetUpdateParams, setTargetUpdateParams] =
+    useState<TargetCreateParams>(initialTargetUpdateParams);
   const field: FieldParams = props.field;
-  const [targetName, setTargetName] = useState<string>(target.targetName);
-  const [targetTemp, setTargetTemp] = useState<number>(target.targetTemp);
-  const [memo, setMemo] = useState<string>(target.memo);
+
+  //const [targetName, setTargetName] = useState<string>(target.targetName);
+  //const [targetTemp, setTargetTemp] = useState<number>(target.targetTemp);
+  //const [memo, setMemo] = useState<string>(target.memo);
+
   const [alertMessageOpen, setAlertMessageOpen] = useState<AlertMessageProps>({
     open: false,
     setOpen: () => {},
@@ -68,29 +77,28 @@ const TargetCard: React.FC<Props> = (props) => {
     message: "",
   });
 
-  const handleChangeTargetName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTargetName(e.target.value);
+  const handleChangeTarget = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTargetUpdateParams(targetUpdateParams);
   };
-  const handleChangeTargetTemp = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTargetTemp(parseInt(e.target.value) || 0);
-  };
-  const handleChangeMemo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMemo(e.target.value);
-  };
+  //const handleChangeTargetTemp = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //  setTargetTemp(parseInt(e.target.value) || 0);
+  //};
+  //const handleChangeMemo = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //  setMemo(e.target.value);
+  //};
 
   const handleTargetUpdate = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const params: TargetCreateParams = {
-      targetName: targetName,
-      targetTemp: targetTemp,
-      memo: memo,
+      targetName: targetUpdateParams.targetName,
+      targetTemp: targetUpdateParams.targetTemp,
+      memo: targetUpdateParams.memo,
     };
 
     try {
       const res = await targetUpdate(params, target.fieldId, target.id);
       console.log(res.data);
       if (res.data.status === "SUCCESS") {
-        setTarget(res.data.data);
         onClickSubmit(e);
       } else {
         setAlertMessageOpen({
@@ -155,12 +163,12 @@ const TargetCard: React.FC<Props> = (props) => {
     <>
       <TargetModal
         title="目標編集"
-        targetName={targetName}
-        targetTemp={targetTemp}
-        memo={memo}
-        onChangeTargetName={handleChangeTargetName}
-        onChangeTargetTemp={handleChangeTargetTemp}
-        onChangeMemo={handleChangeMemo}
+        target={targetUpdateParams}
+        //targetTemp={targetUpdateParams.targetTemp}
+        //memo={targetUpdateParams.memo}
+        onChangeTarget={handleChangeTarget}
+        //onChangeTargetTemp={handleChangeTargetTemp}
+        //onChangeMemo={handleChangeMemo}
         onClickSubmit={(e) => {
           handleTargetUpdate(e);
         }}
