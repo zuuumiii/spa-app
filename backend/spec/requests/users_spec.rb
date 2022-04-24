ef704d289c0519f1b190322859d9c48c2997df97
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-04-24 17:54:27 +0900

    ユーザー削除リクエストテスト追加

--- backend/spec/requests/users_spec.rb
+++ backend/spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Create", type: :request do
+RSpec.describe "SignUp", type: :request do
   before do
     @user = FactoryBot.build(:user)
     @auth_tokens = auth_sign_in(@user)
@@ -35,19 +35,41 @@ RSpec.describe "Create", type: :request do
       end
     end
   end
+end
 
+RSpec.describe "User", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    @auth_tokens = auth_sign_in(@user)
+    @params = {
+      name: @user.name,
+      email: @user.email,
+      password: @user.password,
+      password_confirmation: @user.password_confirmation,
+      prec_no: @user.prec_no,
+      block_no: @user.block_no,
+    }
+  end
   describe "ユーザー削除" do
     context "正しく削除できるとき" do
       it "正しく削除できる" do
-        
+        expect{delete api_v1_user_registration_path(@user), headers: @auth_tokens}.to change{User.count}.by(-1)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("success")
+        expect(res["message"]).to include("アカウントは削除されました")
+        expect(response).to have_http_status :ok
       end
     end
     context "削除できないとき" do
       it "別のユーザから削除しようとすると、エラーが出て削除されない" do
- 
+        
       end
       it "認証されていないユーザーから削除できない" do
-
+        expect{delete api_v1_user_registration_path(@user)}.to change{User.count}.by(0)
+        res = JSON.parse(response.body)
+        expect(res["status"]).to eq("error")
+        expect(res["errors"]).to include("削除するアカウントが見つかりません。")
+        expect(response).to have_http_status 404
       end
     end
   end
